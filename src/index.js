510873a9b65821ad50c44323992ba3a64fbd5da6
mi-szmudzinska <miszmudzinska@gmail.com>
2021-01-04 00:11:03 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import * as THREE from 'three';
-import './main.css';
+//import './main.css';
 import ElementsRepository from './model/ElementsRepository';
 import Atom from './module/Atom';
 
 import { initEnviroment } from './core/core.js';
 
-const {
-	animate,
-	addUpdate,
-	scene,
-  } = initEnviroment()
+// const {
+// 	animate,
+// 	addUpdate,
+// 	scene,
+//   } = initEnviroment()
 
-const repo = new ElementsRepository();
-const element = repo.getByName(repo.showNames()[3])
+// const repo = new ElementsRepository();
+// const element = repo.getByName(repo.showNames()[3])
 
-const atom = new Atom(repo.getByIndex(115));
+// const atom = new Atom(repo.getByIndex(115));
 
-scene.add(atom)
-addUpdate(atom.update);
+// scene.add(atom)
+// addUpdate(atom.update);
 
-animate();
+// animate();
 
 
